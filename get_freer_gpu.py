9d00fe3e3af88a8bfe1b7db654cf7ac618e55019
Chang Wanli <wanlichang@whut.edu.cn>
2020-12-24 15:15:27 +0800

    Create get_freer_gpu.py

---
+++ get_freer_gpu.py
@@ -0,0 +1,8 @@
+import os
+def get_freer_gpu():
+    os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp')
+    memory_available = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
+    free_gpu_list = np.argwhere(memory_available == np.amax(memory_available))
+    for free_gpu in free_gpu_list:
+        if free_gpu != 0:
+            return free_gpu.item()
