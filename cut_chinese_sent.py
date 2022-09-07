21281c67b1b0f449208c8e1050323201d4d2e60c
Chang Wanli <wanlichang@whut.edu.cn>
2022-09-07 11:30:53 +0800

    Create cut_chinese_sent.py

---
+++ cut_chinese_sent.py
@@ -0,0 +1,11 @@
+import re
+def cut_chinese_sent(para):
+    """
+    Cut the Chinese sentences more precisely, reference to "https://blog.csdn.net/blmoistawinde/article/details/82379256".
+    """
+    para = re.sub(r'([。！？\?])([^”’])', r'\1\n\2', para)
+    para = re.sub(r'(\.{6})([^”’])', r'\1\n\2', para)
+    para = re.sub(r'(\…{2})([^”’])', r'\1\n\2', para)
+    para = re.sub(r'([。！？\?][”’])([^，。！？\?])', r'\1\n\2', para)
+    para = para.rstrip()
+    return para.split("\n")
